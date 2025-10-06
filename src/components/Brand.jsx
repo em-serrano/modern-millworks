e5d26388d07a50bdaef57a6cbe4b2f8280cd60c2
Miguel <m9.0serrano@gmail.com>
2025-10-06 00:32:21 -0500

    logo refinement

--- src/components/Brand.jsx
+++ src/components/Brand.jsx
@@ -1,16 +1,15 @@
-// components/Brand.jsx
-import React from 'react';
-import Logo from '/assets/rafRoundel.svg';
+import React from "react";
+import Logo from "/assets/rafRoundel.svg";
 
 export default function Brand() {
   return (
-    <div className="flex items-center whitespace-nowrap">
+    <div className="flex flex-col sm:flex-row items-center justify-center text-center sm:text-left gap-2 sm:gap-4 w-full">
       <img
         src={Logo}
         alt="Modern Millworks Logo"
-        className="h-16 w-15  m-1 shrink-0"
+        className="h-12 w-12 xs:h-14 xs:w-14 sm:h-16 sm:w-16 md:h-20 md:w-20 shrink-0"
       />
-      <h1 className="text-4xl sm:text-5xl lg:text-6xl font-bold text-white tracking-tight font-[Montserrat]">
+      <h1 className="text-3xl xs:text-4xl sm:text-5xl md:text-6xl font-bold text-white tracking-tight font-[Montserrat]">
         Modern Millworks
       </h1>
     </div>
