16edbdbb3cedcf115c8e3b39891f932defd07154
Miguel <m9.0serrano@gmail.com>
2025-10-06 00:23:36 -0500

    Logo adjust

--- src/components/Brand.jsx
+++ src/components/Brand.jsx
@@ -4,11 +4,11 @@ import Logo from '/assets/rafRoundel.svg';
 
 export default function Brand() {
   return (
-    <div className="flex items-center whitespace-nowrap m-5">
+    <div className="flex items-center whitespace-nowrap">
       <img
         src={Logo}
         alt="Modern Millworks Logo"
-        className="h-16 w-16 sm:h-20 sm:w-20 mr-4 shrink-0"
+        className="h-16 w-16 sm:h-20 sm:w-20 items-center shrink-1"
       />
       <h1 className="text-4xl sm:text-5xl lg:text-6xl font-bold text-white tracking-tight font-[Montserrat]">
         Modern Millworks
